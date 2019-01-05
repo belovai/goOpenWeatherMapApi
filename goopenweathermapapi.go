e8e40d61130f0cc48c454d7e126620085f0f9ea3
Arpad Belovai <rp@belovai.com>
2019-01-05 16:13:29 +0100

    GetWeatherByCityCoordinates added

--- goopenweathermapapi.go
+++ goopenweathermapapi.go
@@ -100,6 +100,47 @@ func (c *Client) GetWeatherByCityID(cityID int, units, lang string) (jsonString 
 	return
 }
 
+//GetWeatherByCityCoordinates You can call By geographic coordinates.
+//lat, lon coordinates of the location of your interest.
+//Units possible values are: metric, imperial or empty string.
+//Lang possible values are: ar, bg, ca, cz, de, el, en, fa, fi, fr, gl, hr, hu, it,
+//ja, kr, la, lt, mk, nl, pl, pt, ro, ru, se, sk, sl, es, tr, ua, vi, zh_cn, zh_tw
+func (c *Client) GetWeatherByCityCoordinates(lat, lon float64, units, lang string) (jsonString string, err error) {
+	params := url.Values{}
+
+	params.Add("APPID", c.APPID)
+	params.Add("lat", strconv.FormatFloat(lat, 'f', 2, 64))
+	params.Add("lon", strconv.FormatFloat(lon, 'f', 2, 64))
+	params.Add("lang", lang)
+	params.Add("units", units)
+
+	url := fmt.Sprintf("%sweather?%s", apiURL, params.Encode())
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	defer resp.Body.Close()
+
+	buff := new(bytes.Buffer)
+	buff.ReadFrom(resp.Body)
+	jsonString = buff.String()
+
+	if resp.StatusCode >= 300 {
+		err = fmt.Errorf("API returned with: %s", resp.Status)
+	}
+
+	return
+}
+
 //GetForecastByCityName You can search weather forecast for 5 days with data every 3 hours by city name.
 //Units possible values are: metric, imperial or empty string.
 //Lang possible values are: ar, bg, ca, cz, de, el, en, fa, fi, fr, gl, hr, hu, it,
